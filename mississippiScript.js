82ee24acad4c1fae6f48955eed4b9f9b3f2606c1
Juzmekeke <keerob17@gmail.com>
2025-05-09 08:25:18 -0500

    updated event info mississippi

--- mississippiScript.js
+++ mississippiScript.js
@@ -8,6 +8,12 @@ changeBackgroundButton5.addEventListener("click", () => {
     'url("https://images.stockcake.com/public/8/e/2/8e26bcf5-7ccf-445f-9f63-ac4777c6ab25_large/vintage-car-seaside-stockcake.jpg")';
 });
 
+// paragraph change code 
+let paragraph = document.getElementById("myParagraph");
+    changeBackgroundButton5.addEventListener("click", () => {
+        paragraph.innerHTML = "<h4>Cruisin' The Coast</h4> Is an annual classic car show and festival held on the Mississippi Gulf Coast, known as America's Largest Block Party. It features thousands of antique, classic, and hot rod vehicles, along with rock 'n' roll music, sock hops, and swap meets. The event takes place over several days in October, drawing enthusiasts from across the United States and beyond.<br>October 5-12, 2025";
+      });
+
 // juke joint festival button
 let changeBackgroundButton6 = document.getElementById("btn-jj");
 
@@ -15,6 +21,11 @@ changeBackgroundButton6.addEventListener("click", () => {
   document.body.style.backgroundImage =
     'url("https://www.visittheusa.com/sites/default/files/styles/1_1_12x_360x1360/public/images/hero_media_image/2019-01/922fdfd7c80f5b922e9cfa0d75492bde.jpeg?h=2992ba0a&itok=A591Ndg1")';
 });
+// paragraph change code 
+
+    changeBackgroundButton6.addEventListener("click", () => {
+        paragraph.innerHTML = "<h4>Juke Joint Festival</h4> An annual celebration of music history, art, and culture that brings Clarksdale together for food and fun.<br> April 11, 2026";
+      });
 
 //Tupelo Elvis Festival
 let changeBackgroundButton7 = document.getElementById("btn-tef");
@@ -23,6 +34,11 @@ changeBackgroundButton7.addEventListener("click", () => {
   document.body.style.backgroundImage =
     'url("https://msfarmcountry.com/wp-content/uploads/2025/05/Saturday-Night-2023-1.jpg")';
 });
+// paragraph change code 
+
+    changeBackgroundButton7.addEventListener("click", () => {
+        paragraph.innerHTML = "<h4>Tupelo Elvis Festival</h4> A celebration of the life of the late Elvis Presley complete with impersonations and performances.<br>June 4-8, 2025";
+      });
 
 // Mississippi Science Fest
 let changeBackgroundButton8 = document.getElementById("btn-msf");
@@ -31,3 +47,8 @@ changeBackgroundButton8.addEventListener("click", () => {
   document.body.style.backgroundImage =
     'url("https://atlantasciencefestival.org/wp-content/uploads/2020/03/ASF2019-bigbang-small-1.jpeg")';
 });
+// paragraph change code 
+
+    changeBackgroundButton8.addEventListener("click", () => {
+        paragraph.innerHTML = "<h4>The Mississippi Science Fest</h4> is an annual event hosted by the LeFleur Museum District in Jackson, Mississippi, that aims to engage students, families, and educators in a fun and interactive learning environment promoting science, technology, engineering, and mathematics.<br> September 15-20, 2025";
+      });
