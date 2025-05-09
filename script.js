4fdaba24a461cbfac252d9964848bc2ec1677d9e
Juzmekeke <keerob17@gmail.com>
2025-05-09 00:14:24 -0500

    added paragraph tag to louisiana

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ let changeBackgroundButton1 = document.getElementById("btn-mg");
 changeBackgroundButton1.addEventListener("click", () => {
   document.body.style.backgroundImage =
     'url("https://as1.ftcdn.net/v2/jpg/02/55/43/00/1000_F_255430069_6ijhfvXjLJjTYelxu7NCRmxj6Hfbuogr.jpg")';
+
 });
+
+// paragraph change code 
+let paragraph = document.getElementById("myParagraph");
+    changeBackgroundButton1.addEventListener("click", () => {
+        paragraph.innerHTML = "Everybody loves Mardi Gras!";
+      });
+
+
+
+
+// state display code
+
+// let button2 = document.getElementById('btn-mg');
+// const heading = document.getElementById('heading');
+
+// button.addEventListener('click', function() {
+//   heading.style.display = 'block';
+// });
+
 
 
 // jazz fest button
@@ -15,6 +35,11 @@ changeBackgroundButton2.addEventListener("click", () => {
     'url("https://www.nojazzfest.com/wp-content/uploads/2020/01/culture-1.jpg")';
 });
 
+// change paragraph code
+changeBackgroundButton2.addEventListener("click", () => {
+    paragraph.innerHTML = "Trombones and trumpets everywhere!";
+  });
+
 // TPC button
 
 let changeBackgroundButton3 = document.getElementById("btn-tpc");
@@ -23,6 +48,10 @@ changeBackgroundButton3.addEventListener("click", () => {
   document.body.style.backgroundImage =
     'url("https://golf-pass.brightspotcdn.com/dims4/default/9b54939/2147483647/strip/true/crop/4368x2456+0+228/resize/900x506!/quality/90/?url=https%3A%2F%2Fgolf-pass-brightspot.s3.amazonaws.com%2Fcc%2Fbb%2F093b927624e8548add05e1e7f24b%2Fp.php")';
 });
+// change paragraph code
+changeBackgroundButton3.addEventListener("click", () => {
+    paragraph.innerHTML = "Forrrrrrrrrrrrre!!!";
+  });
 
 // straqwberry fest button
 
@@ -33,7 +62,10 @@ changeBackgroundButton4.addEventListener("click", () => {
     'url("https://images.squarespace-cdn.com/content/v1/6328ba227a155411a8fe15f5/41fd0ef0-095f-4771-87c0-99fe74a42a05/PARADE-min.png")';
 });
 
-
+// change paragraph code
+changeBackgroundButton4.addEventListener("click", () => {
+    paragraph.innerHTML = "Strawberry everything!";
+  });
 
 // FLORIDA
 
