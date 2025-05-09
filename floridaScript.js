ecdab9038eaf35fd467c4b55283091af36249c28
Juzmekeke <keerob17@gmail.com>
2025-05-09 14:09:36 -0500

    added event info florida

--- floridaScript.js
+++ floridaScript.js
@@ -8,6 +8,12 @@ changeBackgroundButton9.addEventListener("click", () => {
     'url("https://media.scurto.net/cdn-cgi/image//1034/media/101262-bullsonthebeach2.jpg")';
 });
 
+// paragraph change code 
+let paragraph = document.getElementById("myParagraph");
+changeBackgroundButton9.addEventListener("click", () => {
+    paragraph.innerHTML = "<h4>Bulls on the Beach</h4> Is a three-night bull riding event held at Flora-Bama in Pensacola. It features professional bull riders from across the country competing in an arena on the beach. The event includes live rodeo action, Kid's Night with coloring contests and bike giveaways, and an adult bull riding competition. A portion of the proceeds benefits St. Jude Children's Research Hospital.<br> September 4-6, 2025";
+  });      
+
 // Tequilla and taco festival button
 let changeBackgroundButton10 = document.getElementById("btn-tt");
 
@@ -15,6 +21,11 @@ changeBackgroundButton10.addEventListener("click", () => {
   document.body.style.backgroundImage =
     'url("https://ilovetheburg.com/wp-content/uploads/2023/05/tacos-tequila-fest-quiero-mas-tacos-1024x576.webp")';
 });
+// paragraph change code 
+
+changeBackgroundButton10.addEventListener("click", () => {
+    paragraph.innerHTML = "<h4>Tacos & Tequila Fest</h4>Is an event celebrating Mexican food and drink, with food vendors serving a variety of tacos, live music, and various tequila tasting experiences. It also includes contests like Best Taco and Best Margarita. <br> February 28, 2026";
+  });
 
 //Digital Grafitti Festival
 let changeBackgroundButton11 = document.getElementById("btn-dgf");
@@ -23,6 +34,11 @@ changeBackgroundButton11.addEventListener("click", () => {
   document.body.style.backgroundImage =
     'url("https://sowal.com/sites/default/files/styles/_bohr-main-image/public/2_74.jpeg?itok=zjJCEYja")';
 });
+// paragraph change code 
+
+changeBackgroundButton11.addEventListener("click", () => {
+    paragraph.innerHTML = "<h4>Digital Graffiti Festival</h4> Is a two-day event that transforms the town's white walls into a digital art canvas. Artists from around the world project their digital creations, including projection mapping, generative art, and animation, creating a unique immersive experience.<br> May 16-17, 2025";
+  });
 
 //Sarasota International Dragon Boat Festival 
 let changeBackgroundButton12 = document.getElementById("btn-sid");
@@ -31,3 +47,8 @@ changeBackgroundButton12.addEventListener("click", () => {
   document.body.style.backgroundImage =
     'url("https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F948943093%2F2600232776711%2F1%2Foriginal.20250130-213107?w=600&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C352%2C4288%2C2144&s=7040c5519c638b2a95b0ef4d4c4f3c9c")';
 });
+// paragraph change code 
+
+changeBackgroundButton12.addEventListener("click", () => {
+    paragraph.innerHTML = "<h4>The Sarasota International Dragon Boat Festival</h4>Is a multi-day event held at Nathan Benderson Park in Sarasota, Florida. The festival combines international dragon boat racing with a cultural expo, health and wellness village, food truck bazaar, and a beer garden. It offers racing for both competitive and recreational teams. <br> May 10, 2025";
+  });
