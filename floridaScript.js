fac009b4eb41fc3827b915363a04d35849ade617
Juzmekeke <keerob17@gmail.com>
2025-05-08 20:55:21 -0500

    added scrips pages and btn links removed Al for index

---
+++ floridaScript.js
@@ -0,0 +1,33 @@
+// FLORIDA
+
+// Bulls on the beach button
+let changeBackgroundButton9 = document.getElementById("btn-bb");
+
+changeBackgroundButton9.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://media.scurto.net/cdn-cgi/image//1034/media/101262-bullsonthebeach2.jpg")';
+});
+
+// Tequilla and taco festival button
+let changeBackgroundButton10 = document.getElementById("btn-tt");
+
+changeBackgroundButton10.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://ilovetheburg.com/wp-content/uploads/2023/05/tacos-tequila-fest-quiero-mas-tacos-1024x576.webp")';
+});
+
+//Digital Grafitti Festival
+let changeBackgroundButton11 = document.getElementById("btn-dgf");
+
+changeBackgroundButton11.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://sowal.com/sites/default/files/styles/_bohr-main-image/public/2_74.jpeg?itok=zjJCEYja")';
+});
+
+//Sarasota International Dragon Boat Festival 
+let changeBackgroundButton12 = document.getElementById("btn-sid");
+
+changeBackgroundButton12.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F948943093%2F2600232776711%2F1%2Foriginal.20250130-213107?w=600&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C352%2C4288%2C2144&s=7040c5519c638b2a95b0ef4d4c4f3c9c")';
+});
---
+++ mississippiScript.js
@@ -0,0 +1,33 @@
+// MISSISSIPPI
+
+// cruisin the coast button
+let changeBackgroundButton5 = document.getElementById("btn-ctc");
+
+changeBackgroundButton5.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://images.stockcake.com/public/8/e/2/8e26bcf5-7ccf-445f-9f63-ac4777c6ab25_large/vintage-car-seaside-stockcake.jpg")';
+});
+
+// juke joint festival button
+let changeBackgroundButton6 = document.getElementById("btn-jj");
+
+changeBackgroundButton6.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://www.visittheusa.com/sites/default/files/styles/1_1_12x_360x1360/public/images/hero_media_image/2019-01/922fdfd7c80f5b922e9cfa0d75492bde.jpeg?h=2992ba0a&itok=A591Ndg1")';
+});
+
+//Tupelo Elvis Festival
+let changeBackgroundButton7 = document.getElementById("btn-tef");
+
+changeBackgroundButton7.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://msfarmcountry.com/wp-content/uploads/2025/05/Saturday-Night-2023-1.jpg")';
+});
+
+// Mississippi Science Fest
+let changeBackgroundButton8 = document.getElementById("btn-msf");
+
+changeBackgroundButton8.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://atlantasciencefestival.org/wp-content/uploads/2020/03/ASF2019-bigbang-small-1.jpeg")';
+});
