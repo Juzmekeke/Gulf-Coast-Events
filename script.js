c72f814a0c177266be734860ba688cf8f0698209
Juzmekeke <keerob17@gmail.com>
2025-05-08 19:15:20 -0500

    added button script info

---
+++ script.js
@@ -0,0 +1,43 @@
+// mardi gras button
+let changeBackgroundButton1 = document.getElementById("btn-mg");
+
+changeBackgroundButton1.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://as1.ftcdn.net/v2/jpg/02/55/43/00/1000_F_255430069_6ijhfvXjLJjTYelxu7NCRmxj6Hfbuogr.jpg")';
+});
+
+
+// jazz fest button
+let changeBackgroundButton2 = document.getElementById("btn-jf");
+
+changeBackgroundButton2.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://www.nojazzfest.com/wp-content/uploads/2020/01/culture-1.jpg")';
+});
+
+// TPC button
+
+let changeBackgroundButton3 = document.getElementById("btn-tpc");
+
+changeBackgroundButton3.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://golf-pass.brightspotcdn.com/dims4/default/9b54939/2147483647/strip/true/crop/4368x2456+0+228/resize/900x506!/quality/90/?url=https%3A%2F%2Fgolf-pass-brightspot.s3.amazonaws.com%2Fcc%2Fbb%2F093b927624e8548add05e1e7f24b%2Fp.php")';
+});
+
+// straqwberry fest button
+
+let changeBackgroundButton4 = document.getElementById("btn-str");
+
+changeBackgroundButton4.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://images.squarespace-cdn.com/content/v1/6328ba227a155411a8fe15f5/41fd0ef0-095f-4771-87c0-99fe74a42a05/PARADE-min.png")';
+});
+
+// cruisin the coast button
+
+let changeBackgroundButton5 = document.getElementById("btn-ctc");
+
+changeBackgroundButton5.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    'url("https://images.stockcake.com/public/8/e/2/8e26bcf5-7ccf-445f-9f63-ac4777c6ab25_large/vintage-car-seaside-stockcake.jpg")';
+});
